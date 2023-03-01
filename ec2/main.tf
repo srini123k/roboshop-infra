679f285e1bd1101d9767dd31c4e9c172c65aed63
Srini K <srinikdevops0131@gmail.com>
2023-03-01 10:01:46 -0800

    updating ./roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -24,7 +24,7 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-      "git clone https://github.com/raghudevopsb71/roboshop-shell",
+      "git clone https://github.com/srini123k/roboshop-shell.git",
       "cd roboshop-shell",
       "sudo bash ${var.component}.sh ${var.password}"
     ]
@@ -58,7 +58,7 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_route53_record" "record" {
-  zone_id = "Z103214126L48SQW30RSR"
+  zone_id = "Z0552687ZOEXRFHA1XPG"
   name    = "${var.component}-dev.devopsb71.online"
   type    = "A"
   ttl     = 30
