14b1284a69317b8b880db6985c456e4e8a562cc3
Srini K <srinikdevops0131@gmail.com>
2023-03-01 09:46:50 -0800

    updating ./roboshop-infra

---
+++ var.tf
@@ -0,0 +1,45 @@
+
+variable "instances" {
+  default = {
+    catalogue={
+      name       = "catalogue"
+      type       = "t3.micro"
+    }
+    user={
+      name       = "user"
+      type       = "t3.micro"
+    }
+    frontend={
+      name       = "frontend"
+      type       = "t3.micro"
+    }
+    mongodb={
+      name       = "mongodb"
+      type       = "t3.micro"
+    }
+    redis={
+      name       = "redis"
+      type       = "t3.micro"
+    }
+    mysql={
+      name       = "mysql"
+      type       = "t3.micro"
+    }
+    payment={
+      name       = "payment"
+      type       = "t3.micro"
+    }
+    cart={
+      name       = "cart"
+      type       = "t3.micro"
+    }
+    shipping={
+      name       = "shipping"
+      type       = "t3.micro"
+    }
+    rabbitmq={
+      name       = "rabbitmq"
+      type       = "t3.micro"
+    }
+  }
+}
