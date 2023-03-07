a86f58a17f6eb4c61cdcb087524876cd6c8844d9
Srini K <srinikdevops0131@gmail.com>
2023-03-07 05:38:41 -0800

    updating ./roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -26,7 +26,7 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-      "ansible-pull -i localhost, -U git clone https://github.com/srini123k/roboshop-ansible.git roboshop.yml -e role_name=${var.component}"
+      "ansible-pull -i localhost, -U https://github.com/srini123k/roboshop-ansible.git roboshop.yml -e role_name=${var.component}"
     ]
   }
 
