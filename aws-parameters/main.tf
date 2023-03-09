c24d92e598030c1f98fa17b6f1d5891b8fbe77e6
Srini K <srinikdevops0131@gmail.com>
2023-03-08 16:22:59 -0800

    updating ./roboshop-infra

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -5,5 +5,13 @@ resource "aws_ssm_parameter" "parameter" {
   value = var.parameters[count.index].value
 }
 
+resource "aws_ssm_parameter" "secret" {
+  count = length(var.secrets)
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].type
+  value = var.parameters[count.index].value
+}
 
 variable "parameters" {}
+
+variable "secrets" {}
