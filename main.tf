53317f2f2559b32e83dc922bc31d330e7c2f586d
Srini K <srinikdevops0131@gmail.com>
2023-03-19 17:49:09 -0700

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ module "vpc" {
   source = "git::https://github.com/srini123k/tf-module-vpc.git"
   env=var.env
   tags=var.tags
+  default_route_table = var.default_route_table
+  default_vpc_id      = var.default_vpc_id
 
   for_each = var.vpc
   vpc_cidr=each.value["vpc_cidr"]
