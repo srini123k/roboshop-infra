06aacc0917aa4912c0f80f5f4e4d172a1df8edc4
Srini K <srinikdevops0131@gmail.com>
2023-04-28 07:49:31 -0700

    updating ./roboshop-infra

--- data.tf
+++ data.tf
@@ -3,8 +3,8 @@ data "aws_ssm_parameter" "ssh_pass" {
 }
 
 
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "devops-practice-with-ansible"
-  owners      = ["self"]
-}
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex  = "devops-practice-with-ansible"
+#  owners      = ["self"]
+#}
